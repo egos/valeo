870c9d77e815cdfde9833d7196d6eaae34f72ca1
Bertrand Pelletier <pelletierber@gmail.com>
2022-08-01 00:30:11 +0200

    crossover v1

--- app.py
+++ app.py
@@ -4,26 +4,81 @@ import numpy as np
 import networkx as nx
 import itertools 
 import math
+import time
 from math import factorial as f
 from datetime import timedelta
 from streamlit import session_state
 import matplotlib.pyplot as plt
 import json
 import collections
+import copy
 from utils import *
 
 # $C^{k-1}_{n+k-1} = \frac{(n+k-1)!}{n! (k-1)!}$
 st.set_page_config(page_title = "_IHM", layout="wide")
 print('BEGIN')
 
-data, dfs, dfline , df1 = load_data(500)
-st._legacy_dataframe(df1)
+
+data, dfs, dfline , df1 = load_data(10, time.ctime())
+
+
+if 'df1' not in session_state:
+
+    df1['Epoch'] = df1.index
+    session_state['df1'] = df1
+else :
+    df1 = session_state['df1']
+
+if 'Epoch' not in session_state:
+    Epoch = df1.index.max()+1
+    session_state['Epoch'] = Epoch
+else : 
+    Epoch = session_state['Epoch']
+
+c1,c2 = st.columns(2)
+
+if c2.button('RESET'):
+    data, dfs, dfline , df1 = load_data(10, time.ctime())
+    df1['Epoch'] = df1.index
+    session_state['df1'] = df1
+    Epoch = df1.index.max()+1
+    session_state['Epoch'] = Epoch
+    
+    
+if c1.button('RUN'):
+    L = []      
+    List = df1[:7].index.values
+    np.random.shuffle(List)
+    print(List)
+    for n in range(3):
+        print(n)
+        i1 = List[n*2]
+        i2 = List[n*2 + 1]
+        dfx = df1.loc[[i1,i2]].copy()
+        L2 = Reprodution(dfx,dfs, dfline)
+        if L2:
+            for l in L2:
+                l.append(Epoch)
+                l.append(dfx.Epoch.tolist())
+                Epoch+= 1
+                L.append(l)
+        
+    col = ['Clist', 'CtoE','Econnect','Elist','Ecount', 'EtoP','Pconnect','Plist','Pcount', 'ID_CtoE','ID_EtoP','dist','Epoch','Parents']
+    dfx = pd.DataFrame(L, columns= col)
+    # dfx
+    df1 = df1.append(dfx)
+    df1['Name'] = (df1.ID_CtoE + df1.ID_EtoP).str.join(',')
+    df1 = df1.drop_duplicates(subset='Name').sort_values('dist').reset_index(drop = True)    
+    session_state['df1'] = df1
+    session_state['Epoch'] = Epoch
+
+st._legacy_dataframe(df1.drop(columns = ['ID_CtoE','ID_EtoP']))
 ListSelectbox = df1.index
-index = st.selectbox('pop',ListSelectbox)
+index = st.selectbox('individu',ListSelectbox)
 row = df1.loc[index]
 
-print(row)
-st._legacy_dataframe(row.to_frame().T)
+# print(row)
+# st._legacy_dataframe(row.to_frame().T)
 ElemsList = ['Clist','Elist','Plist']
 Elems = ['C','E','P']
 IDSelects = []
@@ -40,30 +95,7 @@ if c2.checkbox('ALL combinaison') :
     dflineSelect = dfline.copy()
     dfsSelect = dfs.copy()
 
-
-N = data['height']
-A0 = data['layers'][0]['data']
-A0 = np.array(A0).reshape(10,7)
-pas = 16
-unique = np.unique(A0)
-A0[A0 == unique[0]] = 0
-A0[A0 == unique[1]] = 1
-for idx, row in dfsSelect.iterrows():
-    A0[row.y, row.x] = row.Color + int(row.Name)
-
-A = np.kron(A0, np.ones((16,16), dtype=int))
-fig, ax = plt.subplots(figsize = (10,10))
-ax.imshow(A)
-for idx, row in dflineSelect.iterrows():
-    p = row.polyline
-    f = ax.plot(p[:,0],p[:,1],'k')
-
-style = dict(size=10, color='black') 
-for idx, row in dfsSelect.iterrows():
-    x = row.x*16
-    y = row.y*16
-    text = row.Class + str(row.Name)
-    f = ax.text(row.x*16+8, row.y*16+8,text , **style,  ha='center', weight='bold') 
+fig = plot_(data,dflineSelect, dfsSelect)
      
 c2._legacy_dataframe(dflineSelect)  
 c2._legacy_dataframe(dfsSelect) 
--- utils.py
+++ utils.py
@@ -10,9 +10,13 @@ from streamlit import session_state
 import matplotlib.pyplot as plt
 import json
 import collections
+import copy
 
 @st.cache(allow_output_mutation=True)
-def load_data(Size):
+def load_data(Size, time):
+    return load_data_brut(Size)
+
+def load_data_brut(Size):
     with open('VALEO_1.tmj', 'r') as f:
       data = json.load(f)
 
@@ -71,3 +75,84 @@ def load_data(Size):
     df['Name'] = (df.ID_CtoE + df.ID_EtoP).str.join(',')
     df1 = df.drop_duplicates(subset='Name').sort_values('dist')
     return data, dfs, dfline , df1.drop(columns = 'Name').reset_index(drop= True)
+
+def plot_(data,dflineSelect, dfsSelect): 
+    N = data['height']
+    A0 = data['layers'][0]['data']
+    A0 = np.array(A0).reshape(10,7)
+    pas = 16
+    unique = np.unique(A0)
+    A0[A0 == unique[0]] = 0
+    A0[A0 == unique[1]] = 1
+    for idx, row in dfsSelect.iterrows():
+        A0[row.y, row.x] = row.Color + int(row.Name)   
+        
+    A = np.kron(A0, np.ones((16,16), dtype=int))
+    fig, ax = plt.subplots(figsize = (10,10))
+    ax.imshow(A)
+    for idx, row in dflineSelect.iterrows():
+        p = row.polyline
+        f = ax.plot(p[:,0],p[:,1],'k')
+
+    style = dict(size=10, color='black') 
+    for idx, row in dfsSelect.iterrows():
+        x = row.x*16
+        y = row.y*16
+        text = row.Class + str(row.Name)
+        f = ax.text(row.x*16+8, row.y*16+8,text , **style,  ha='center', weight='bold') 
+    return fig
+
+def indiv_verif(row,NewCtoE,dfs, dfline): 
+    D = dfs.Class.value_counts().to_dict()
+    Clist = list(range(D['C']))
+    
+    CtoE = NewCtoE
+    Econnect = dict(collections.Counter(CtoE))
+    Elist = sorted(Econnect)
+    Ecount = len(Elist)   
+    
+    if Ecount > row.Ecount:
+        x = row.EtoP
+        y = np.random.randint(0,D['P'],Ecount - row.Ecount)
+        EtoP = np.append(x, y)
+    elif Ecount < row.Ecount:
+        EtoP = np.random.choice(row.EtoP,row.Ecount - Ecount)
+        print(EtoP)
+    else : 
+        EtoP = row.EtoP
+
+    Pconnect = dict(collections.Counter(EtoP))
+    Plist = sorted(Pconnect)
+    Pcount = len(Plist)    
+
+    ID_CtoE = ['C-E{}{}'.format(i, CtoE[i]) for i in Clist]
+    ID_EtoP = ['E-P{}{}'.format(Elist[i]  , EtoP[i]) for i in range(Ecount)]
+
+    dist = dfline.loc[dfline.ID.isin(ID_CtoE + ID_EtoP), 'dist'].sum()  
+    l = [Clist, CtoE,Econnect,Elist,Ecount, EtoP,Pconnect,Plist,Pcount, ID_CtoE,ID_EtoP,dist]
+    return l 
+
+def Reprodution(dfx,dfs, dfline):
+    c1, c2 = copy.deepcopy(dfx.CtoE.values)
+    
+    if (c1 != c2).any():        
+        m = c1 != c2
+        index = np.where(m)[0]
+        
+        if len(index) > 1:    
+            idx = np.random.choice(index)
+        elif len(index) ==1:
+            idx = index            
+#         print(i1,i2,c1, c2,m, idx)    
+        c1[idx] , c2[idx] = c2[idx] , c1[idx]
+        NewCtoE = c1 , c2
+#         print(NewCtoE)
+        L = []
+
+        for i in range(2):
+            row = dfx.iloc[i]
+#             print(row.to_list())
+            l = indiv_verif(row,NewCtoE[i],dfs, dfline) 
+#             print(l) 
+            L.append(l)
+        return L
